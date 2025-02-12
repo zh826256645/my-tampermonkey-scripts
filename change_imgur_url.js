088da5446a136bd13a2d47e6367f3c763bbb9eac
钟浩 <zh826256645@gmail.com>
2025-02-12 14:36:28 +0800

    feat(v2ex): 替换图片链接为镜像站

---
+++ change_imgur_url.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         修改网站 imgur 的链接
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  由于 imgur 无法访问，修改为镜像网站
+// @author       XiGuaShu
+// @match        https://v2ex.com/*
+// @run-at document-end
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    // 获取当前网页中 body 的所有 img 标签
+	const imgElements = document.getElementsByTagName("img");
+
+	// 遍历每个 img 标签
+	for (let i = 0; i < imgElements.length; i++) {
+        const imgElement = imgElements[i];
+        const src = imgElement.getAttribute('src');
+
+        // 检查 img 标签的 src 是否以 /img/ 开头
+        if (src.startsWith('https://i.imgur.com/') || src.startsWith('https://imgur.com/')) {
+            imgElement.setAttribute('src', `https://img.noobzone.ru/getimg.php?url=${src.replace("https://imgur.com/", "https://i.imgur.com/")}`);
+            imgElement.setAttribute('referrerPolicy', 'no-referrer');
+        }
+	}
+}());
